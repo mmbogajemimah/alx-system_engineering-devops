677a8d8a45d1f0f82c7cb1bc312e4df4f839677e
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-11-30 15:36:01 +0300

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,7 @@
-# Fixing typing error in word press settings
+# Fixing wordpress typo settings
 
 exec { 'fix-wordpress':
-  command => 'sed =i s/phpp/php/g /var/www/html/wp-settings.php',
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
 }
 
